bfc6057173533aef5479f20ea12aab1d3fe96f79
REJWAN <76392817+Consultant777@users.noreply.github.com>
2025-07-31 23:52:01 +0600

    Add files via upload

---
+++ class.py
@@ -0,0 +1,6 @@
+import logging 
+    logging.basicConfig(filename=' rejwan.log', level = logging.INFO)
+    logging.debug("this is a debug msg")
+    logging.info("this is a information msg ")
+    logging.warning("this is a warning msg")
+    logging.error("this is a error msg")
